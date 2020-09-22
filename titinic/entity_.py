65afd9b1a6954d5a79b4a3d5e015062e0445dff0
Kim <fkqoseka@gmail.com>
2020-09-22 10:55:21 +0900

    10:55

--- titinic/entity_.py
+++ titinic/entity_.py
@@ -1,15 +1,11 @@
-from dataclasses import dataclass
-
-
-@dataclass
 class Entity:
-
-    context: str
-    fname: str
-    train: object
-    text: object
-    id: str
-    label: str
+    def __init__(self, context, fname, train, text, id, label):
+        self._context = context  # _ 는 default 접근, __ 2개는 pritave 접근 의미
+        self._fname = fname
+        self._train = train
+        self._text = text
+        self._id = id
+        self._label = label
 
     @property
     def context(self) -> str:
