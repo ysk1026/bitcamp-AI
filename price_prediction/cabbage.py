5a7de5e8d8aed066ddffd9cf179968cb293df946
Kim <fkqoseka@gmail.com>
2020-09-25 09:46:30 +0900

    09:46

--- price_prediction/cabbage.py
+++ price_prediction/cabbage.py
@@ -7,34 +7,42 @@ import tensorflow as tf
 
 class Model:
     def __init__(self):
-        self.filereader = FileReader()
-        
+        self.fileReader = FileReader() 
+        self.context = '/Users/youngseonkim/Documents/SbaProjects/price_prediction/data/'
     def new_model(self, payload) -> object:
-        this = self.filereader
-        this.context = '/Users/youngseonkim/Documents/SbaProjects/price_prediction/data/'
+        this = self.fileReader
         this.fname = payload
-        return pd.read_csv(this.context + this.fname, sep=',') 
-    
+        return pd.read_csv(self.context + this.fname, sep=',')
+
     def create_tf(self, payload):
         xy = np.array(payload, dtype=np.float32)
         x_data = xy[:,1:-1] # feature
         y_data = xy[:,[-1]] # price
-        x = tf.placeholder(tf.float32, shape=[None, 4])
-        y = tf.placeholder(tf.float32, shape=[None, 2])
-        w = tf.Variable(tf.random_normal([4, 1]), name = 'weight')
-        b = tf.Variable(tf.random_normal([1]), name = 'bias')
-        hypothesis = tf.matmul(x, w) + b
-        cost = tf.reduce_mean(tf.square(hypothesis - y))
-        optimizer = tf.train.GradientDescentOptimizer(learning_rate = 0.000005)
+        X = tf.compat.v1.placeholder(tf.float32, shape=[None, 4])
+        Y = tf.compat.v1.placeholder(tf.float32, shape=[None, 1])
+        W = tf.Variable(tf.random.normal([4, 1]), name='weight')
+        b = tf.Variable(tf.random.normal([1]), name='bias')
+        hyposthesis = tf.matmul(X, W) + b
+        cost = tf.reduce_mean(tf.square(hyposthesis - Y))
+        optimizer = tf.compat.v1.train.GradientDescentOptimizer(learning_rate=0.000005)
         train = optimizer.minimize(cost)
-        sess = tf.Session()
-        sess.run(tf.global_variables_initializer())
+        sess = tf.compat.v1.Session()
+        sess.run(tf.compat.v1.global_variables_initializer())
         for step in range(100000):
-            cost_, hypo_, = sess.run([cost, hypothesis, train], feed_dict ={x : x_data, y: y_data})
+            cost_, hypo_, _ = sess.run([cost, hyposthesis, train],
+                                        feed_dict={X: x_data, Y: y_data})
             if step % 500 == 0:
-                print(f'# {step} 손실 비용 : {cost_}')
+                print(f'# {step} 손실비용: {cost_} ')
                 print(f'- 배추가격 : {hypo_[0]}')
-                
-        saver = tf.train.Saver()
-        print('저장 완료')
 
+        saver = tf.compat.v1.train.Saver()
+        saver.save(sess, self.context +'saved_model.ckpt')
+        print('저장완료')
+
+
+if __name__ == '__main__':
+    m= Model()
+    dframe = m.new_model('price_data.csv')
+    print(dframe.head())
+    m.create_tf(dframe)
+    
