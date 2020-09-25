e954b1a8bf146883e3c519601c923d352a9059c8
Kim <fkqoseka@gmail.com>
2020-09-25 12:29:51 +0900

    12:30

--- app.py
+++ app.py
@@ -1,12 +1,38 @@
 from flask import Flask
-
+from flask import render_template, request
+from price_prediction.cabbage import Cabbage
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello Python !'
+    return render_template('index.html')
 
 
+# method 는 총 4개가 있습니다.
+# GET, POST, PUT, DELETE 그래서 이것들은 array를 이뤄요.
+# 그 array 이름은 methods 입니다.
+# app 에서 POST 라고 하면 인지하지 못하고 ['POST'] 해야 인지합니다.
+# 그래서 아래 코드는
+# methods = ['POST']로 바뀝니다.
+# 이런 methods 를 RESTful 이라고 합니다.
+@app.route('/cabbage', methods =["POST"])
+def cabbage():
+    print('UI to API Connect Success')
+    avgTemp = request.form['avgTemp']
+    minTemp = request.form['minTemp']
+    maxTemp = request.form['maxTemp']
+    rainFall = request.form['rainFall']
+    print(f'avgTemp : {avgTemp}')
+    print(f'minTemp : {minTemp}')
+    print(f'maxTemp : {maxTemp}')
+    print(f'rainFall : {rainFall}')
+    cabbage = Cabbage()
+    cabbage.avgTemp = avgTemp
+    cabbage.minTemp = minTemp
+    cabbage.maxTemp = maxTemp
+    cabbage.rainFall = rainFall
+    result = cabbage.service()
+    print(f'Prediction : {result}')
 if __name__ == '__main__':
     app.run()
--- price_prediction/cabbage.py
+++ price_prediction/cabbage.py
@@ -4,8 +4,22 @@ from util.file_handler import FileReader
 import pandas as pd
 import numpy as np
 import tensorflow as tf
+from dataclasses import dataclass
 
-class Model:
+@dataclass
+class Cabbage:
+    
+    # year, avgTemp, minTemp, maxTemp, rainFall, avgPrice
+    # 20100101, -4.9, -11, 0.9, 0, 2123
+    # 멤버 변수
+    year : int = 0
+    avgTemp: float = 0.0
+    minTemp: float = 0.0
+    maxTemp: float = 0.0
+    rainFall: float = 0.0
+    avgPrice: int = 0
+    
+    # 클래스 내부에서 공유하는 객체, 상수값
     def __init__(self):
         self.fileReader = FileReader() 
         self.context = '/Users/youngseonkim/Documents/SbaProjects/price_prediction/data/'
@@ -39,10 +53,41 @@ class Model:
         saver.save(sess, self.context +'saved_model.ckpt')
         print('저장완료')
 
+    def test(self):
+        self.avgPrice = 100
+        return self.avgPrice
+    
+    def service(self):
+        X = tf.compat.v1.placeholder(tf.float32, shape=[None, 4])
+        # year, avgTemp, minTemp, maxTemp, rainFall, avgPrice
+        # 에서 avgTemp, minTemp, maxTemp, rainFall 입력 받겠다
+        # year 는 모델에서 필요없는 값 -> 상관 관계 없음
+        # avgPrice 는 얻고자 하는 답
+        # avgTemp, minTemp, maxTemp, rainFall, avgPrice 는 종속변수를 결정하는 독립변수
+        # 그리고 avgPrice 를 결정하는 요소로 사용되는 파라머티 (이것이 중요!)
+        # 이제 우리는 통계와 확률로 들어가야 합니다. 용어를 먼저 잘 정의합시다.
+        # y = ax + b 선형관계 linear ...
+        # X 는 대문자를 사용하고 확률 변수라고 합니다
+        # 비교, 웹프로그래밍(Java, C) 소문자 x 이렇게 하는데 이것은 한 타임에 하나의 value
+        # 그리고 그 값은 외부에서 주어지는 하나의 값이므로 그냥 변수
+        # 이럴때는 확률 --- 변수
+        W = tf.Variable(tf.random.normal([4, 1]), name = 'weight')
+        b = tf.Variable(tf.random.normal([1]), name ='bias')
+        saver = tf.train.Saver()
+        with tf.Session() as sess:
+            sess.run(tf.compat.v1.global_variables_initializer())
+            saver.restore(sess, self.context+'saved_model.ckpt')
+            data = [[self.avgTemp, self.minTemp, self.maxTemp, self.rainFall],]
+            arr = np.array(data, dtype = np.float32)
+            dict = sess.run(tf.matmul(X, W) + b, ({X: arr[0:4]}))
+            # Y = WX + b 를 코드로 표현하면 위 처럼
+            print(dict[0])
+        return int(dict[0])
 
 if __name__ == '__main__':
-    m= Model()
-    dframe = m.new_model('price_data.csv')
-    print(dframe.head())
-    m.create_tf(dframe)
-    
+    cabbage = Cabbage()
+    # dframe = m.new_model('price_data.csv')
+    # print(dframe.head())
+    # m.create_tf(dframe)
+    m.avgPrice = 1
+    print(cabbage.avgPrice)
